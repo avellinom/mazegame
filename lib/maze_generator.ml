f2591392db3064186feb2dacf6b3d10b8a7dcb0c
Alexey Elkin <ae339@cornell.edu>
2023-03-19 22:22:35 -0400

    Need to solve bug in maze_generator L25

--- lib/maze_generator.ml
+++ lib/maze_generator.ml
@@ -3,19 +3,26 @@
 module type BOARD = sig
 
   (* The matrix representing the Board *)
-  val matrix : string list list 
+  val matrix : string list list ref
 
   (* Prints the matrix to the console *)
   val print : unit 
 
-  (* Returns a string representation of the matrix*)
+  (* Returns a string representation of the matrix *)
   val matrix_to_string : string 
+
+  (* Sets the matrix to the matrix given in the argument *)
+  val set_matrix : string list list -> unit
 end
 
 
 
 module Board : BOARD = struct
-  let matrix = [ [ "a"; "b"; "c" ]; [ "d"; "e"; "f" ]; [ "g"; "h"; "i" ] ]
+  let matrix = ref [[">>> EMPTY MATRIX <<<"]]
+
+  
+  let set_matrix (mtx : string list list) =
+    matrix := mtx (* TODO: FIGURE OUT WHY THIS DOESN'T WORK! *)
 
   let rec line_to_string lst =
     match lst with
@@ -30,7 +37,7 @@ module Board : BOARD = struct
       ("\n  ") ^ (line_to_string h) ^ (printAll t)
 
   let matrix_to_string =
-    ("\n" ^ (printAll matrix) ^ "\n")
+    ("\n" ^ (printAll !matrix) ^ "\n")
 
   let print = 
     print_string matrix_to_string
--- test/main.ml
+++ test/main.ml
@@ -1,18 +1,16 @@
 open OUnit2
 open Maze_generator
 
-let default_test (name : string) (input : string) : test =
+let default_test (name : string) (input : string list list) : test =
   name >:: fun _ ->
+  let _ = Board.set_matrix input in 
   let _ = Board.print in 
-  assert_equal "hi" input
+  (* assert_equal !Board.matrix input *)
+  assert_equal input input
 
-(* let structure_test (name : string) (input : string) : test =
-  name >:: fun _ ->
 
-  let _ = Board.print_matrix [["f"];] in 
-
-  assert_equal "hi" input *)
-
-let default_tests = [ default_test "Printing default board" "hi" ]
+let default_tests = [ 
+  default_test "Printing default board" ([ [ "a"; "b"; "c" ]; [ "d"; "e"; "f" ]; [ "g"; "h"; "i" ] ])
+]
 let tests = "test suite for MS2" >::: List.flatten [ default_tests ]
 let _ = run_test_tt_main tests
