cf004ade5795ffdaae9483e6c0d47a90705c2299
mca63 <mca63@cornell.edu>
2023-05-13 13:19:53 -0400

    Added substantial testing for maze traversal. 	- achieved 50+ tests

--- test/main.ml
+++ test/main.ml
@@ -50,53 +50,228 @@ let solved_game_test (name : string)
     (solved_move_sequence : unit -> Controller.t) : test =
   name >:: fun _ -> assert_raises MazeSolved solved_move_sequence
 
+(* This sequence puts the player one above the goal on the large maze. *)
+let get_close_to_small_maze_exit (image_amount : int) : Controller.t =
+  make_game Small image_amount
+  |> move_down |> move_right |> move_right |> move_right |> move_right
+  |> move_down |> move_down |> move_left |> move_left |> move_down |> move_down
+  |> move_right |> move_right |> move_down |> move_down |> move_right
+  |> move_right |> move_right |> move_right |> move_right |> move_right
+  |> move_right |> move_right |> move_right |> move_down |> move_down
+  |> move_right |> move_right
+
+(* This sequence puts the player one above the goal on the large maze. *)
+let get_close_to_large_maze_exit (image_amount : int) : Controller.t =
+  make_game Large image_amount
+  |> move_right |> move_right |> move_down |> move_down |> move_down
+  |> move_right |> move_right |> move_up |> move_up |> move_right |> move_right
+  |> move_right |> move_right |> move_right |> move_right |> move_right
+  |> move_right |> move_right |> move_down |> move_down |> move_left
+  |> move_left |> move_left |> move_down |> move_down |> move_right
+  |> move_right |> move_right |> move_down |> move_down |> move_down
+  |> move_down |> move_down |> move_down |> move_down |> move_down |> move_left
+  |> move_left |> move_down |> move_down |> move_down |> move_down |> move_right
+  |> move_right |> move_right |> move_down |> move_down |> move_right
+  |> move_right |> move_right |> move_right |> move_right |> move_up |> move_up
+  |> move_right |> move_right |> move_right |> move_right |> move_down
+  |> move_down |> move_right |> move_right |> move_right |> move_right
+  |> move_right |> move_right |> move_right
+
 let controller_tests =
   [
-    valid_game_test "User can go right on small maze"
+    (* Emmpty maze simple inital movement *)
+    valid_game_test "User can go right initially on empty maze"
+      (make_game Empty 0 |> move_right)
+      false;
+    valid_game_test "User can go down initially on empty maze"
+      (make_game Empty 0 |> move_down)
+      false;
+    invalid_move_game_test "User cannot move left initially on empty maze"
+      (fun () -> make_game Empty 0 |> move_left);
+    invalid_move_game_test "User cannot move up initially on empty maze"
+      (fun () -> make_game Empty 0 |> move_up);
+    (* Small maze simple initial movement *)
+    valid_game_test "User can go right initially on small maze"
       (make_game Small 0 |> move_right)
       false;
-    valid_game_test "User can approach barrier on small maze"
+    valid_game_test "User can go down initially on small maze"
+      (make_game Small 0 |> move_down)
+      false;
+    invalid_move_game_test "User cannot move left initially on small maze"
+      (fun () -> make_game Small 0 |> move_left);
+    invalid_move_game_test "User cannot move up initially on small maze"
+      (fun () -> make_game Small 0 |> move_up);
+    (* Large maze simple initial movement *)
+    valid_game_test "User can go right initially on large maze"
+      (make_game Large 0 |> move_right)
+      false;
+    valid_game_test "User can go down initially on large maze"
+      (make_game Large 0 |> move_down)
+      false;
+    invalid_move_game_test "User cannot move left initially on large maze"
+      (fun () -> make_game Large 0 |> move_left);
+    invalid_move_game_test "User cannot move up initially on large maze"
+      (fun () -> make_game Large 0 |> move_up);
+    (* Mazes can be generated with images *)
+    valid_game_test "Empty maze can have images dropped on it"
+      (make_game Empty 3) false;
+    valid_game_test "Small maze can have images dropped on it"
+      (make_game Small 3) false;
+    valid_game_test "Large maze can have images dropped on it"
+      (make_game Large 3) false;
+    (* Mazes can be generated with more images than available maze locations*)
+    valid_game_test "Empty maze survives with extreme image count"
+      (make_game Empty 10000) false;
+    valid_game_test "Small maze can be constructed with extreme image count"
+      (make_game Small 10000) false;
+    valid_game_test "Large maze can be constructed with extreme image count"
+      (make_game Large 10000) false;
+    (* It is OK for a user to be adjacent to a barrier on its right *)
+    valid_game_test "User can approach barrier from left on empty maze"
+      (make_game Empty 0 |> move_down |> move_right)
+      false;
+    valid_game_test "User can approach barrier from left on small maze"
       (make_game Small 0 |> move_right |> move_right)
       false;
-    valid_game_test "User can move down on small maze"
-      (make_game Small 0 |> move_right |> move_down |> move_right)
+    valid_game_test "User can approach barrier from left on large maze"
+      (make_game Large 0 |> move_right |> move_right)
       false;
-    invalid_move_game_test "User cannot move into wall" (fun () ->
+    (* It is OK for a user to be adjacent to a barrier above it *)
+    valid_game_test "User can approach barrier from beneath it on empty maze"
+      (make_game Empty 0 |> move_down |> move_down |> move_right |> move_right)
+      false;
+    valid_game_test "User can approach barrier from beneath it on small maze"
+      (make_game Small 0 |> move_down |> move_right |> move_right |> move_right)
+      false;
+    valid_game_test "User can approach barrier from beneath it on large maze"
+      (make_game Large 0 |> move_right |> move_down |> move_down |> move_down
+     |> move_right |> move_right)
+      false;
+    (* It is OK for a user to be adjacent to a barrier beneath it *)
+    valid_game_test "User can approach barrier from above it on empty maze"
+      (make_game Empty 0 |> move_right |> move_right)
+      false;
+    valid_game_test "User can approach barrier from above it on small maze"
+      (make_game Small 0 |> move_down)
+      false;
+    valid_game_test "User can approach barrier from above it on large maze"
+      (make_game Large 0 |> move_down)
+      false;
+    (* It is OK for a user to be adjacent to a barrier on its left *)
+    valid_game_test "User can approach barrier from its right on empty maze"
+      (make_game Empty 0 |> move_right |> move_right |> move_right |> move_down)
+      false;
+    valid_game_test "User can approach barrier from its right on small maze"
+      (make_game Small 0 |> move_down |> move_right |> move_right |> move_right
+     |> move_right |> move_down)
+      false;
+    valid_game_test "User can approach barrier from its right on large maze"
+      (make_game Large 0 |> move_right |> move_down |> move_down)
+      false;
+    (* A user cannot move into a wall on its right *)
+    invalid_move_game_test
+      "User cannot move into wall on its right on empty maze" (fun () ->
+        make_game Empty 0 |> move_down |> move_right |> move_right);
+    invalid_move_game_test
+      "User cannot move into wall on its right on small maze" (fun () ->
         make_game Small 0 |> move_right |> move_right |> move_right);
-    solved_game_test
-      "Empty maze is easily solvable by tracing left and bottom edges"
+    invalid_move_game_test
+      "User cannot move into wall on its right on large maze" (fun () ->
+        make_game Large 0 |> move_right |> move_right |> move_right);
+    (* A user cannot move into a wall above it *)
+    invalid_move_game_test "User cannot move into wall above it on empty maze"
       (fun () ->
         make_game Empty 0 |> move_down |> move_down |> move_right |> move_right
-        |> move_right |> move_right);
-    solved_game_test
-      "Empty maze is easily solvabe by tracing top and right edges while the \
-       maze is filled with images" (fun () ->
-        make_game Empty 5 |> move_right |> move_right |> move_right
-        |> move_right |> move_down |> move_down |> move_down);
-    solved_game_test
-      "More images than available board spaces does not cause problems"
+        |> move_up);
+    invalid_move_game_test "User cannot move into wall above it on small maze"
       (fun () ->
-        make_game Empty 10000 |> move_right |> move_right |> move_right
-        |> move_right |> move_down |> move_down |> move_down);
-    invalid_move_game_test "User cannot move into wall on their right"
-      (fun () -> make_game Empty 0 |> move_down |> move_right |> move_right);
-    invalid_move_game_test "User cannot move into wall beneath them" (fun () ->
-        make_game Empty 0 |> move_right |> move_right |> move_down);
-    invalid_move_game_test "User cannot move into wall on their left" (fun () ->
+        make_game Small 0 |> move_down |> move_right |> move_right |> move_right
+        |> move_up);
+    invalid_move_game_test "User cannot move into wall above it on large maze"
+      (fun () ->
+        make_game Large 0 |> move_right |> move_down |> move_down |> move_down
+        |> move_right |> move_right |> move_up);
+    (* A user cannot move into a wall beneath it *)
+    invalid_move_game_test "User cannot move into wall beneath it on empty maze"
+      (fun () -> make_game Empty 0 |> move_right |> move_right |> move_down);
+    invalid_move_game_test "User cannot move into wall beneath it on small maze"
+      (fun () -> make_game Small 0 |> move_down |> move_down);
+    invalid_move_game_test "User cannot move into wall beneath it on large maze"
+      (fun () -> make_game Large 0 |> move_down |> move_down);
+    (* A user cannot move into a wall on its left *)
+    invalid_move_game_test
+      "User cannot move into wall on its left on empty maze" (fun () ->
         make_game Empty 0 |> move_right |> move_right |> move_right |> move_down
         |> move_left);
-    invalid_move_game_test "User cannot move into wall on their right"
-      (fun () -> make_game Empty 0 |> move_down |> move_right |> move_right);
-    invalid_move_game_test "User cannot move off board by moving left"
-      (fun () -> make_game Large 0 |> move_left);
-    invalid_move_game_test "User cannot move off board by moving up" (fun () ->
-        make_game Large 0 |> move_up);
-    invalid_move_game_test "User cannot move off board by moving down"
-      (fun () -> make_game Large 0 |> move_down |> move_down |> move_down);
-    invalid_move_game_test "User cannot move off board by moving right"
-      (fun () ->
-        make_game Large 0 |> move_right |> move_right |> move_right
+    invalid_move_game_test
+      "User cannot move into wall on its left on small maze" (fun () ->
+        make_game Small 0 |> move_right |> move_right |> move_down |> move_right
+        |> move_right |> move_down |> move_left);
+    invalid_move_game_test
+      "User cannot move into wall on its left on large maze" (fun () ->
+        make_game Large 0 |> move_right |> move_down |> move_down |> move_left);
+    (* A user cannot move off the board by moving right *)
+    invalid_move_game_test
+      "User cannot move off the board by moving right on empty maze" (fun () ->
+        make_game Empty 0 |> move_right |> move_right |> move_right
         |> move_right |> move_right);
+    invalid_move_game_test
+      "User cannot move off the board by moving right on small maze" (fun () ->
+        get_close_to_small_maze_exit 0 |> move_right);
+    invalid_move_game_test
+      "User cannot move off the board by moving right on large maze" (fun () ->
+        get_close_to_large_maze_exit 0 |> move_right);
+    (* A user cannot move off the board by moving up *)
+    invalid_move_game_test
+      "User cannot move off the board by moving up on empty maze" (fun () ->
+        make_game Empty 0 |> move_right |> move_up);
+    invalid_move_game_test
+      "User cannot move off the board by moving up on small maze" (fun () ->
+        make_game Small 0 |> move_right |> move_up);
+    invalid_move_game_test
+      "User cannot move off the board by moving up on large maze" (fun () ->
+        make_game Large 0 |> move_right |> move_up);
+    (* A user cannot move off the board by moving down *)
+    invalid_move_game_test
+      "User cannot move off the board by moving down on empty maze" (fun () ->
+        make_game Empty 0 |> move_down |> move_down |> move_down);
+    invalid_move_game_test
+      "User cannot move off the board by moving down on small maze" (fun () ->
+        get_close_to_small_maze_exit 0 |> move_left |> move_down |> move_down);
+    invalid_move_game_test
+      "User cannot move off the board by moving down on large maze" (fun () ->
+        get_close_to_large_maze_exit 0 |> move_left |> move_down |> move_down);
+    (* A user cannot move off the board by moving left *)
+    invalid_move_game_test
+      "User cannot move off the board by moving left on empty maze" (fun () ->
+        make_game Empty 0 |> move_down |> move_left);
+    invalid_move_game_test
+      "User cannot move off the board by moving left on small maze" (fun () ->
+        make_game Small 0 |> move_down |> move_left);
+    invalid_move_game_test
+      "User cannot move off the board by moving left on large maze" (fun () ->
+        make_game Large 0 |> move_down |> move_left);
+    (* Solvability *)
+    solved_game_test "User can solve empty maze by tracing top and right edges"
+      (fun () ->
+        make_game Empty 0 |> move_right |> move_right |> move_right
+        |> move_right |> move_down |> move_down);
+    solved_game_test
+      "User can solve empty maze by tracing left and bottom edges" (fun () ->
+        make_game Empty 0 |> move_down |> move_down |> move_right |> move_right
+        |> move_right |> move_right);
+    solved_game_test "User can solve small maze" (fun () ->
+        get_close_to_small_maze_exit 0 |> move_down);
+    solved_game_test "User can solve large maze" (fun () ->
+        get_close_to_large_maze_exit 0 |> move_down);
+    (* Solvability with images *)
+    solved_game_test "User can solve empty maze with images" (fun () ->
+        make_game Empty 5 |> move_down |> move_down |> move_right |> move_right
+        |> move_right |> move_right);
+    solved_game_test "User can solve small maze with images" (fun () ->
+        get_close_to_small_maze_exit 10 |> move_down);
+    solved_game_test "User can solve large maze with images" (fun () ->
+        get_close_to_large_maze_exit 25 |> move_down);
   ]
 
 let tests = "test suite for MS2" >::: List.flatten [ controller_tests ]
