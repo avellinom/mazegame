4bdb766fd38bddce8fbddb3178402ccb9c0bc28c
Jun <jj578@cornell.edu>
2023-05-15 13:12:26 -0400

    Update image.make_random

--- image/main.ml
+++ image/main.ml
@@ -48,8 +48,6 @@ let test_stuff () =
 
 let () =
   open_graph " 800x800";
-  set_color (palette (Skyblue 1));
-  fill_rect 0 0 1000 1000;
   make_random_tree ();
   ignore (wait_next_event [ Button_down ]);
   close_graph ()
--- src/image.ml
+++ src/image.ml
@@ -6,8 +6,6 @@ type t = unit -> unit
 let make_random () : t =
  fun () ->
   open_graph " 800x800";
-  set_color (palette (Skyblue 1));
-  fill_rect 0 0 1000 1000;
   make_random_tree ();
   ignore (wait_next_event [ Button_down ]);
   close_graph ()
